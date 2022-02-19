fd83c2dc58955c39642978431842c82ff643f16d
faruq ismael <farusmith5@gmail.com>
2022-02-18 22:55:56 -0800

    folder structure

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { connect } from "mongoose";
+import gui from "./routes/gui";
+
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.use("/static", express.static("public"));
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/gui", gui);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+function connectDB() {
+  try {
+    connect("mongodb://localhost:27017/ridy-test", () => {
+      console.log("db connected 🔥");
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
