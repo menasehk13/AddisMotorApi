41fe9e9835448b5c5465f97f6de7de42cc143796
menasehk <menasehk38@gmail.com>
2022-02-22 10:20:39 +0300

    connection update

--- src/helpers/connection.js
+++ src/helpers/connection.js
@@ -1,5 +1,21 @@
 import { db_uri } from "./constants";
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
 
 export default function connectDB() {
-  console.log("db connected 🔥");
+  dotenv.config()
+  
+  // FIXME: 
+  //sotenv not working
+   mysql.createConnection({
+    host:'192.168.5.8',
+    user:'Wedetadmin',
+    password: 'p@55w0rd' ,
+    database:'Driver',
+  }).connect((err)=>{
+    if (err) return err
+    console.log("db connected 🔥");
+  })
+  
+ 
 }
