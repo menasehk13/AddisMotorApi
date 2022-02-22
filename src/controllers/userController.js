5c1065b5b64bc154c26b7db066564a77a7f4a795
menasehk <menasehk38@gmail.com>
2022-02-22 10:41:12 +0300

    db update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 // get users
 
 import { catchAsync } from "../middlewares/error";
-
+import connectDB from "../helpers/connection";
 // get users
 const getUsers = catchAsync(async (req, res, next) => {
   res.send(" getting users from Controller");
@@ -9,13 +9,26 @@ const getUsers = catchAsync(async (req, res, next) => {
 
 // get user
 const getUser = catchAsync(async (req, res, next) => {
+
   res.send("from Controller");
 });
 
 // create/register user
+const createUser =  catchAsync(async (req, res, next) => {
+ 
+  const data = req.body;
+    try {
+        
+    } catch (error) {
+      
+    }
 
+})
 // update user
+const updateUser = catchAsync(async (req, res, next) => {
 
+
+})
 // delete user
 
 export default {
--- src/helpers/connection.js
+++ src/helpers/connection.js
@@ -4,18 +4,21 @@ import dotenv from 'dotenv'
 
 export default function connectDB() {
   dotenv.config()
-  
+
   // FIXME: 
   //sotenv not working
-   mysql.createConnection({
+  const db= mysql.createConnection({
     host:'192.168.5.8',
     user:'Wedetadmin',
     password: 'p@55w0rd' ,
     database:'Driver',
-  }).connect((err)=>{
+  })
+  db.connect((err)=>{
     if (err) return err
     console.log("db connected 🔥");
   })
+
+  return db;
   
  
 }
