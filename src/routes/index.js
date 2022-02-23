8f5002fbc22d6495f0262ee2409beaadf5e14ab0
faruq ismael <farusmith5@gmail.com>
2022-02-22 23:16:48 -0800

    user -> users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { catchGlobalError, catchUnregisteredUrl } from "../middlewares/error";
 import userRoute from "./userRoute";
+import driverRoute from "./driverRoute";
 import authRoute from "./authRoute";
 
 export default (app, api) => {
   app.use(api + "/auth", authRoute);
-  app.use(api + "/user", userRoute);
-  //   app.use(api + "/location", locationRoute);
-  //   app.use(api + "/level", levelRoute);
+  app.use(api + "/users", userRoute);
+  app.use(api + "/drivers", driverRoute);
 
   // url doesnt exist -> fallback
   app.all("*", catchUnregisteredUrl);
