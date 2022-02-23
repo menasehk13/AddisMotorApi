00cfbe6a504475cb12b35384ce4f7659cc8b4bfa
faruq ismael <farusmith5@gmail.com>
2022-02-23 00:29:10 -0800

    restrict to specific roles

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -102,8 +102,22 @@ const protect = catchAsync(async (req, res, next) => {
   });
 });
 
+const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'operator']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+
+    next();
+  };
+};
+
 export default {
   login,
   protect,
   register,
+  restrictTo,
 };
