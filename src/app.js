e587b4f3f87b572e64c149021f008be7bbf40a53
menasehk <menasehk38@gmail.com>
2022-04-17 18:49:18 +0300

    socket update

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
