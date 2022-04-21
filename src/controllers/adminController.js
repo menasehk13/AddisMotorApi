103038f5c13d726ac6537e0703fd9e52e69860b1
menasehk <menasehk38@gmail.com>
2022-04-21 22:56:55 +0300

    update admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -14,7 +14,7 @@ const DB = connectDB();
 
 // get users
 const currentAdmin = catchAsync(async (req, res, next) => {
-  const {id} = jwt.decode(req.query.token);
+  const id = req.query.id;
   DB.query(`SELECT * FROM Admin WHERE id=${id} LIMIT 1`, function (err, results, fields) {
     if (err) return next(new AppError(err.message, 400));
     return res.json({ user: results });
