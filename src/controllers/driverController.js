d729b9af38139c28b04c84df5e63693741943779
menasehk <menasehk38@gmail.com>
2022-04-26 17:36:45 +0300

    update values

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -235,7 +235,7 @@ const BuyNewCredit = catchAsync(async (req,res,next)=>{
         return res.json(results[0].id)
        }
        
-       const idUsed = results.map((items)=> items.id)
+       const idUsed = results[0].id
        const currencyValue= 123123123123
        console.log(idUsed[0].id)
        return res.json({"status":"Fail"})
