92efb653489b860247d1c6aed4a259b20eb0ddd4
menasehk <menasehk38@gmail.com>
2022-04-26 17:07:03 +0300

    commit driver change

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -231,7 +231,10 @@ const BuyNewCredit = catchAsync(async (req,res,next)=>{
      DB.query(driverQuery.BuyCredit(currencyid),(err,results)=>{
        if(err) return next(new AppError(err.message,400))
        console.log(results)
-       return res.json(results)
+       if(results && results.length>0 ){
+        return res.json(results)
+       }
+        return res.json({"status":"Fail"})
       // else if(results &&  results.length < 0) return res.json({"status":"Currency already been Used !!!!!"})
       //  const id = results.insertId
       //  const currencyValue= 123123123123
