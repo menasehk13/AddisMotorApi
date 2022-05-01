b119d268023b7de8c4a64688f0edd80ef114d6ce
menasehk <menasehk38@gmail.com>
2022-05-02 00:03:24 +0300

    update 2

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -278,7 +278,7 @@ const updatestatus = catchAsync(async(req,res,next)=>{
 
 const DispatchToDriver = catchAsync(async (req,res,next)=>{
   const id =req.query.id
-  console.log(req.body.picLat)
+  console.log(req.body.pickLat)
   DB.query(driverQuery.getdriver(id),(err,results)=>{
     if(err) next(new AppError(err.message,404))
       const socketid = results[0].socketid
