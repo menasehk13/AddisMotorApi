5051ca4fa0078153affc2dcc60270211bb33c325
menasehk13 <menasehk38@gmail.com>
2022-06-08 22:18:51 +0300

    check

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -24,6 +24,7 @@ console.log(getids)
     }
 }
 export async function NotificationSingle(title,message,id){
+    console.log(id)
     const notification = {
         headings:{
         'en':title
@@ -31,7 +32,7 @@ export async function NotificationSingle(title,message,id){
         contents:{
             'en': message,
         },
-        include_player_ids: [id],
+        include_player_ids: [`${id}`],
         template_id: 'a0cb3093-d5b9-4197-ae04-ccf44f335b2f',
     }
     
